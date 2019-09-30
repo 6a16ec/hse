b56889a1ea307029a710840b5d981a07c04001dd
6a16ec <semaev2014@yandex.ru>
2019-09-30 08:07:17 +0300

    Create insertion_sort.cpp

---
+++ algorithms/insertion_sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+#define debug 0
+
+vector <int> insertion(vector <int> array){
+    for(int i = 1; i < array.size(); i++){
+        int number = array[i], index = i;
+        for(int j = i - 1; j >= 0; j--){
+            if (array[j] >  number){
+                array[j + 1] = array[j];
+                index -= 1;
+            }
+            else break;
+        }
+        array[index] = number;
+    }
+
+
+    return array;
+}
+
+int main() {
+    if (debug == 0){
+        int n = 0; cin >> n;
+        vector <int> array(n);
+        for(int i = 0; i < n; i++)
+            cin >> array[i];
+        array = insertion(array);
+        for(int i = 0; i < array.size(); i++)
+            cout << array[i] << " ";
+    }
+    else{
+        setlocale(LC_ALL, "");
+        vector <int> array{338, 391, 12030, 101, 1999, 572, 1886, 1690, 189, 2619, 1281, 369};
+        array = insertion(array);
+        for(int i = 0; i < array.size(); i++)
+            cout << array[i] << " ";
+    }
+
+
+    return 0;
+}
